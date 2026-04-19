125ad1c13a5ae65c35612cfeee6d079628c14b08
Zachary Scheer <zscheer00@gmail.com>
2026-04-19 14:49:43 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int p: prices){
+            if(p < minPrice){
+                minPrice = p;
+            }//if
+            else{
+                maxProfit = Math.max(maxProfit, p - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
