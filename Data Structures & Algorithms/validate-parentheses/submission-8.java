f196cc26caa0da488f02ab0542d15aafed126dc5
Zachary Scheer <zscheer00@gmail.com>
2026-04-20 09:41:22 -0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for(char r : chars){
+            if(r == '(' || r == '{' || r == '['){
+                stack.push(r);
+            }//if
+            else if(r == ')' || r == '}' || r == ']'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char compare = stack.pop();
+                if((compare == '(' && r != ')') ||
+                   (compare == '[' && r != ']')||
+                    (compare == '{' && r != '}'))
+                {
+                    return false;
+                }
+                
+            }
+        }
+
+
+        return stack.isEmpty();
+    }
+}
