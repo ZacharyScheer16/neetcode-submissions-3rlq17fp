27314d8fc2fd06fcd4b92da2c7dd4152d2eb8a82
Zachary Scheer <zscheer00@gmail.com>
2026-07-17 22:34:46 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int islands = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (grid[r][c] == '1') {
+                    islands++;
+                    dfs(grid, r, c);
+                }
+
+            }
+        }
+
+        return islands;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+
+        // check boundary
+        if (r < 0 || r >= grid.length ||
+            c < 0 || c >= grid[0].length) {
+            return;
+        }
+
+        // water or already visited
+        if (grid[r][c] == '0') {
+            return;
+        }
+
+        // mark visited
+        grid[r][c] = '0';
+
+        // explore neighbors
+        dfs(grid, r + 1, c); // down
+        dfs(grid, r - 1, c); // up
+        dfs(grid, r, c + 1); // right
+        dfs(grid, r, c - 1); // left
+    }
+}
